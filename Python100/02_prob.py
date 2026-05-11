759ce069a8c6623f67b89f8e82a758cd44858aff
Skikbal <skikbalali02@gmail.com>
2026-05-11 16:16:25 +0530

    Day-1 of practicing problems

---
+++ Python100/02_prob.py
@@ -0,0 +1,10 @@
+import math 
+# find the sqrt of a number using exponitiation
+num = 64
+sqroot = 64**0.5
+print(f"The squre root of the number is {sqroot}")
+
+#with Math module
+num = int(input("Enter the number: "))
+sqrot = math.sqrt(num)
+print(f"The squre root of number is {sqrot}")
