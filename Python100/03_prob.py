df4a232ce914f6676f2e460ad41aa7f33bc409e2
Skikbal <skikbalali02@gmail.com>
2026-05-12 14:45:50 +0530

    day 2 of python practice

---
+++ Python100/03_prob.py
@@ -0,0 +1,5 @@
+# python programn to calcullate the area of a triangle
+base = float(input("Enter the measurement of the base of the traingle: "))
+height = float(input("Enter the measurement of height of the traingle: "))
+area = (0.5) * base * height
+print(f"The Area of the traingle is: {area}")
