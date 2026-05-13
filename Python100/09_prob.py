f3563411d810f0cce72eebda85ea29bf674d81ab
Skikbal <skikbalali02@gmail.com>
2026-05-13 14:30:11 +0530

    day 3 of practicing python

---
+++ Python100/09_prob.py
@@ -0,0 +1,10 @@
+# program to check if the number is posetive negetive or zero
+
+number = int(input("Enter the number: "))
+
+if number > 0:
+    print("The number is posetive")
+elif number < 0:
+    print("The number is negetive")
+else:
+    print("The number is zero")
