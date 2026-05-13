6eaae3aabe3495cb21ecefd1dc7570de7f599201
Skikbal <skikbalali02@gmail.com>
2026-05-13 14:29:50 +0530

    day 3 of practicing python

---
+++ Python100/08_prob.py
@@ -0,0 +1,6 @@
+# celsius to faranahit
+
+celsius = float(input("Enter the temperature in celsius:   "))
+
+fahrenheit = (celsius * 1.8) + 32
+print(f"The temperature in fahrenheit is {fahrenheit}")
