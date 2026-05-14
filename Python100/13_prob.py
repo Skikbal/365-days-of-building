2d81ac3b38f08331f76a0226c4f7d5cf44826d91
Skikbal <skikbalali02@gmail.com>
2026-05-14 15:19:01 +0530

    day 4 practicing python and stay consistent

--- Python100/13_prob.py
+++ Python100/13_prob.py
@@ -1,1 +1,14 @@
- 
+num = int(input("Enter the number: "))
+isPrime = True
+if num <= 1:
+    isPrime = False
+else:
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0 :
+            isPrime = False
+            break
+
+if isPrime:
+    print(num,"is prime")
+else:
+    print(num,"is not prime")
