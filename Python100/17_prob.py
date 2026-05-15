640198061704239c07f34def18e4bcf6dcfe790d
Skikbal <skikbalali02@gmail.com>
2026-05-15 21:52:36 +0530

    day 5 practicing python and stay consistent

--- Python100/17_prob.py
+++ Python100/17_prob.py
@@ -3,3 +3,10 @@ num = int(input("Enter the number: "))
 for i in range (1,num+1):
     for j in range (1,11):
         print(f"{i} x {j} = {i*j}")
+        
+
+# using while loop find table of a number
+i = 1
+while i <= 10:
+    print(f"{num} x {i} = {num*i}")
+    i = i+1
