546d0b21959aed6e7e235d12f3ae6fac44251b72
Skikbal <skikbalali02@gmail.com>
2026-05-16 22:29:41 +0530

    day 6 practicing python and stay consistent

--- Python100/21_prob.py
+++ Python100/21_prob.py
@@ -3,4 +3,6 @@
 nterms = int(input("Enter number of terms: "))
 
 result = list(map(lambda x: 2**x, range(nterms+1)))
-print(result)
+
+for i in range(nterms+1):
+    print("2 raised to power",i,"is",result[i])
