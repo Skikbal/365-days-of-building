6219bf463a32c8d515321d31283b01df12119010
Skikbal <skikbalali02@gmail.com>
2026-05-17 22:51:50 +0530

    day 7 practicing python and stay consistent

--- Python100/25_prob.py
+++ Python100/25_prob.py
@@ -1,3 +1,30 @@
-# print the sum of the natural number
-# natural number s are the all the posetive integer
+# PROGRAM TO FIND HCF OR GCD
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
+# def calcHCF(num1,num2):
+#     smaller=0
+#     hcf = 0
+#     if num1 > num2:
+#         smaller = num2
+#     else:
+#         smaller = num1
+        
+#     for i in range(1,smaller+1):
+#         if ((num1%i == 0) and (num2%i == 0)):
+#             hcf = i
+#     return hcf
+# result = calcHCF(num1,num2)
+# print("The hcf is ", result)
+
+
+def calc_hcf(num1,num2):
+    smaller = min(num1,num2)
+    
+    for i in range(smaller,0,-1):
+        if num1 % i == 0 and num2 % i == 0:
+            return i
+
+result = calc_hcf(num1,num2)
+
+print(result)
