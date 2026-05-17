156c80ab80660faf97999989438f858ce1e9d262
Skikbal <skikbalali02@gmail.com>
2026-05-17 23:09:18 +0530

    day 7 practicing python and stay consistent

--- Python100/28_prob.py
+++ Python100/28_prob.py
@@ -1,6 +1,7 @@
 import random,itertools
 
 deck = list(itertools.product(range(1,14),["Spades","Hearts","Diamonds","Clubs"]))
-result = random.choice(deck)
+random.shuffle(deck)
 
-print(result)
+for i in range(5):
+    print(deck[i][0],"of",deck[i][1])
