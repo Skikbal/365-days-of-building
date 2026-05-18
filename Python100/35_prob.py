2bfbfa85a5000badabb65c7e8a779f728e19c30c
Skikbal <skikbalali02@gmail.com>
2026-05-18 23:10:35 +0530

    day 8 practicing python and stay consistent

--- Python100/35_prob.py
+++ Python100/35_prob.py
@@ -1,7 +1,9 @@
-n = int(input("Enter the number: "))
-sum = 0
-for i in range (1,n+1):
-    sum += i
-    
-    
-print(sum)
+lower = int(input("Enter the lower limit: "))
+upper = int(input("Enter the upper limit: "))
+
+for i in range(lower,upper+1):
+    for j in range(2,i):
+        if i % j == 0:
+            print(i)
+    else:
+        continue
