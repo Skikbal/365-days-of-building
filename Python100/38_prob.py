70a4344a4f035f0387905fe833746a930a404226
Skikbal <skikbalali02@gmail.com>
2026-05-19 23:52:54 +0530

    day 9 of 100 python program

---
+++ Python100/38_prob.py
@@ -0,0 +1,13 @@
+# multiplay two matrix
+
+matrix1 = [1,2,3],[4,5,6],[7,8,9]
+matrix2 = [9,8,7],[6,5,4],[3,2,1]
+
+result = [[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[i])):
+        for k in range(len(matrix2)):
+         result[i][j] += matrix1[i][k]*matrix2[k][j]
+
+print(result)
