18e6fe9a66b9dc6366ad682049ee60fb67b6712c
Skikbal <skikbalali02@gmail.com>
2026-05-20 00:11:38 +0530

    day 9 of 100 python program

--- Python100/40_prob.py
+++ Python100/40_prob.py
@@ -1,1 +1,10 @@
-# matrix transpose
+# palindrome check
+
+text = "madam"
+
+reverse_text = text[::-1]
+
+if text == reverse_text:
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
